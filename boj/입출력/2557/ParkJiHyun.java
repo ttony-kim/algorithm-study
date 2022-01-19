12b859ce89ea38ce1f1810533cb99a7d2f32d805
jyhynn <letsjamm@naver.com>
2022-01-19 11:14:52 +0900

    test

--- boj/입출력/2557/ParkJiHyun.java
+++ boj/입출력/2557/ParkJiHyun.java
@@ -3,7 +3,7 @@ package home.algorithm;
 public class ParkJiHyun {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! ");
 	}
 
 }
