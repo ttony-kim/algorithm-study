18f43e92403147d33a1b85f9dae0f0d2446fc869
leelee31 <lilychouchou31@gmail.com>
2022-02-07 21:57:31 +0900

    백준 그리디 2839 설탕 배달 풀이 추가

---
+++ boj/그리디/2839/LeeSuMin.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sugar = Integer.parseInt(br.readLine());
+
+		int result = -1;
+		int m = 1, n = 0;
+		
+		// 우선 순위 3
+		if(sugar % 3 == 0) {
+			result = sugar / 3;
+		}
+		
+		// 우선 순위 2
+		while(m == sugar / 3) {
+			n = sugar - 3 * m;
+			if(n % 5 == 0) {
+				result = m + n / 5;
+				break;
+			}
+			m++;
+		}
+
+		m = sugar / 5; n = 0;
+
+		// 우선 순위 1
+		while(m > 0) {
+			n = sugar - 5 * m;
+			if(n % 3 == 0) {
+				result = m + n / 3;
+				break;
+			}
+			m--;
+		}
+		
+		System.out.println(result);
+	}
+}
