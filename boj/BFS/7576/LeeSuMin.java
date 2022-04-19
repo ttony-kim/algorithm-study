d19e99cbed262bc78f120765047a68a50f237585
leesumin <lilychouchou31@gmail.com>
2022-04-20 01:00:28 +0900

    처음부터 익은 토마토를 전부 큐에 먼저 넣고 bfs 실행

--- boj/BFS/7576/LeeSuMin.java
+++ boj/BFS/7576/LeeSuMin.java
@@ -6,89 +6,71 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int M, N;
+    static int[][] box;
+    static Queue<Pos> q;
+    static int[] dX = {1, -1, 0, 0};
+    static int[] dY = {0, 0, 1, -1};
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-
-        boolean isNothingToRotten = true;
+        box = new int[N][M];
+        q = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
-            int[] line = new int[M];
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j ++) {
-                line[j] = Integer.parseInt(st.nextToken());
-                if(line[j] == 0 && isNothingToRotten) {
-                    isNothingToRotten = false;
-                }
-            }
-            map[i] = line;
-        }
-
-        if(isNothingToRotten) {
-            System.out.println(0);
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            for(int j = 0; j< M; j++) {
-                if (map[i][j] == 1) {
-                    bfs(i, j);
+            for (int j = 0; j < M; j++) {
+                box[i][j] = Integer.parseInt(st.nextToken());
+                if (box[i][j] == 1) {
+                    Pos pos = new Pos(i, j, 0);
+                    q.add(pos);
                 }
             }
         }
+
+        bfs();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                //System.out.print(map[i][j]);
-                if (map[i][j] == 0) {
+                if (box[i][j] == 0) {
                     System.out.println(-1);
                     return;
                 }
             }
-            //  System.out.println();
         }
 
-        System.out.println(totalDay);
+        System.out.println(max > 0 ? max : 0);
     }
 
     static class Pos {
         int x, y, day;
-        Pos (int x, int y, int day) {
+
+        Pos(int x, int y, int day) {
             this.x = x;
             this.y = y;
             this.day = day;
         }
     }
 
-    static int M, N;
-    static int[][] map;
-    static int totalDay;
-    static int[] dX = { 1, -1, 0, 0 };
-    static int[] dY = { 0, 0, 1, -1 };
-
-    private static void bfs (int x, int y) {
-        Queue<Pos> q = new LinkedList<>();
-        Pos pos = new Pos(x, y, 0);
-        q.add(pos);
+    private static void bfs() {
         while (!q.isEmpty()) {
             Pos p = q.poll();
-            //System.out.println("polled P : " + p.x + " , " + p.y);
-            for (int i=0; i<4; i++) {
+            for (int i = 0; i < 4; i++) {
                 int newX = p.x + dX[i];
                 int newY = p.y + dY[i];
                 int newD = p.day + 1;
-                if (newX < 0 || newX >= N || newY < 0 || newY >= M || map[newX][newY] != 0) continue;
+                if (newX < 0 || newX >= N || newY < 0 || newY >= M || box[newX][newY] != 0) continue;
                 Pos newP = new Pos(newX, newY, newD);
                 q.add(newP);
-                //System.out.println("new pos push!" + "newX: " + newX + " , newY : " + newY + ", newday: " + newD);
-                map[newX][newY] = newD;
-                totalDay = Math.max(totalDay, newD);
+                box[newX][newY] = 1;
+                if (max < newD) {
+                    max = newD;
+                }
             }
-            //System.out.println();
         }
     }
 }
