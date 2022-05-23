8ecf5cbc7d00b5521848eeca0a8d9d118dcb2935
DASOM <ektha115@gmail.com>
2022-05-23 18:18:57 +0900

    mod: 2558

--- boj/입출력/2558/KimDaSom.java
+++ boj/입출력/2558/KimDaSom.java
@@ -1,53 +1,17 @@
 package boj.입출력;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
-public class problem_10951 {
+public class problem_2558 {
 
 	public static void main(String[] args) {
-		//Scanner 방식
-		/*Scanner sc = new Scanner(System.in);
-		int a, b;
-		while(sc.hasNextInt()) {
-			a = sc.nextInt();
-			b = sc.nextInt();
-			System.out.println(a+b);
-		}
-		sc.close();*/
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
 		
-		//BufferedReader 방식, StringTokenizer 사용
-		/*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String line;
-			while((line = br.readLine()) != null) {
-				StringTokenizer st = new StringTokenizer(line, " ");
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				System.out.println(a+b);
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}*/
-		
-		//BufferedReader 방식, charAt 사용
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String line;
-			while((line = br.readLine()) != null) {
-				int a = line.charAt(0)-48;
-				int b = line.charAt(2)-48;
-				System.out.println(a+b);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		System.out.println(a+b);
+
 	}
 
 }
