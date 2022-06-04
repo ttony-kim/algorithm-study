02fb6dc8ebe3b7257c09745224d6ae6b6c1202bd
dskim <69332203+ttony-kim@users.noreply.github.com>
2022-06-04 20:03:00 +0900

    Create KimDaSom.java
    new: 10991

---
+++ boj/입출력/10991/KimDaSom.java
@@ -0,0 +1,35 @@
+package boj.입출력;
+
+import java.util.Scanner;
+
+public class problem_10991 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i ++) {
+			for(int j = 0; j < N-(i+1); j++) {
+				sb.append(" ");
+			}
+			for(int j = 0; j <= i; j ++) {
+				if(j % 2 == 0) {
+					sb.append("*");
+				} else {
+					sb.append(" ");
+				}
+			}
+			for(int j = 0; j < i; j ++) {
+				if((i+j)%2 != 0) {
+					sb.append("*");
+				} else {
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
